f51a078375fab9e0fb74a90ee26e4e60a0d68468
Nutdanai Kumsiri <Nutdanai.ku@ku.th>
2021-11-09 23:44:09 +0700

    Demo-Swagger

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace demoSwaggerAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : Controller
+    {
+        [HttpGet]
+        public Product Get()
+        {
+            return new Product
+            {
+                IdProduct = 123,
+                NameProduct = "Microchip-A",
+                PriveProduct = 30
+            };
+        }
+
+        [HttpGet("GetProduct")]
+        public IEnumerable<Product> GetProducts()
+        {
+            return new List<Product>();
+        }
+
+        [HttpPost]
+        public Product CreateNewProduct([FromBody] Product product)
+        {
+            // product.IdProduct = 999;
+
+            return product;
+        }
+
+        [HttpPut]
+        public string ChangeProduct([FromBody] Product product)
+        {
+            return "Product has been changed";
+        }
+    }
+}
---
+++ Model/Product.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace demoSwaggerAPI.Controllers
+{
+    public class Product
+    {
+        public int IdProduct { get; set;}
+        public string NameProduct {get; set;}
+        public double PriveProduct {get; set;}
+    }
+}
